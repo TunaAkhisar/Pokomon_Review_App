7d31b3a43068e48d21e2e4b6257ec41f9e69fb05
Tunahan Akhisar <tunaakhisar9@gmail.com>
2023-10-25 13:59:23 +0300

    Owner; Repository, Dto, Map, Controller, Review; Repository, Dto, Map, Controller, Reviewer; Repository, Dto, Map, Controller, Dependency Injection for owner,review,reviewer
    Are Added

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddTransient<Seed>();
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+builder.Services.AddScoped<IReviewerRepository, ReviewerRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ PokemonReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,44 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _context;
+
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Owner GetOwner(int ownerId)
+        {
+            return _context.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokeId)
+        {
+            return _context.PokemonOwners.Where(p => p.PokemonId == pokeId)
+                .Select(o => o.Owner)
+                .ToList();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.OrderBy(o => o.Id).ToList();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            return _context.PokemonOwners.Where(p => p.OwnerId == ownerId)
+                .Select(p => p.Pokemon) .ToList();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _context.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
